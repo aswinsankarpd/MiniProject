26caebfedbcee583e45020463fca84404159decf
Aswin Sankar P D <you@example.com>
2022-06-28 14:20:22 +0530

    Voice added

---
+++ texttospeech.py
@@ -0,0 +1,17 @@
+import win32com.client as wincl
+from time import sleep
+import asyncio
+
+def texttospeech(word):
+    speaker_number = 1
+    spk = wincl.Dispatch("SAPI.SpVoice")
+    vcs = spk.GetVoices()
+    SVSFlag = 11
+    print(vcs.Item (speaker_number) .GetAttribute ("Name")) # speaker name
+    spk.Voice
+    spk.SetVoice(vcs.Item(speaker_number)) # set voice (see Windows Text-to-Speech settings)
+    spk.Speak(word)
+
+def wordappender(word_array,letter):
+    word_array.append(letter)
+    print(letter)
